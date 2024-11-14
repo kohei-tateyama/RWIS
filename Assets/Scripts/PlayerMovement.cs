a068dbfbbf2dd1470f683a9a31882bd3cf7f9dc3
kohei-tateyama <koheitateyama@keio.jp>
2024-11-14 21:30:55 +0900

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+
+    public float moveSpeed = 8f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
