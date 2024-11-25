3ca23465e00db8532af46c9cf4afe430bc742ff1
kohei-tateyama <koheitateyama@keio.jp>
2024-11-26 08:48:08 +0900

    healthbar

---
+++ Assets/SocialMeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SocialMeter : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxMeter(int socialPoint)
+    {
+        slider.maxValue = socialPoint;
+        slider.value = socialPoint;
+    }
+
+    public void SetMeter(int socialPoint)
+    {
+        slider.value = socialPoint;
+    }
+}
